30366e361bfe3eb5bf90871d50cf3b8bcba6dbdd
Robert Hargreaves <kiyouta@gmail.com>
2025-08-03 14:27:54 +0100

    Fix build

--- noughts-and-crosses/include/board.hpp
+++ noughts-and-crosses/include/board.hpp
@@ -1,11 +1,8 @@
-
 #pragma once
 #include <span>
 #include "cell.hpp"
 
 class Board {
-
 	public:
 		std::span<Cell> getBoard();
-
 };
--- noughts-and-crosses/src/main.cpp
+++ noughts-and-crosses/src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "board.hpp"
 
 int main() {
-    std::cout << "Greetings Professor Falken" << std::endl;
+    Board board;
+    auto cells = board.getBoard();
+    for (const auto& cell : cells) {
+        char state_char;
+        switch (cell.getState())
+        {
+        case Cell::State::EMPTY:
+            state_char = ' ';
+            break;
+        case Cell::State::X:
+            state_char = 'X';
+            break;
+        case Cell::State::O:
+            state_char = 'O';
+            break;
+        default:
+            state_char = '?';
+            break;
+        }
+        std::cout << state_char << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
